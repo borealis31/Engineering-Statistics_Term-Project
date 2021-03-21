022ef654939dafc11e2fae94fc60c0fbb66ea8dd
borealis31 <jakeb.chouinard@gmail.com>
2021-03-20 21:37:04 -0400

    Implemented regexp techniques and cellfun methods to optimize object

--- Controller.m
+++ Controller.m
@@ -1,4 +1,4 @@
 clear
-book_shelf(1) = book('text_files/pp.txt');
-book_shelf(2) = book('text_files/staveone.txt');
+book_shelf(1) = book('text_files/pp.txt')
+book_shelf(2) = book('text_files/staveone.txt')
 %sum(vertcat(book_shelf.mu))
